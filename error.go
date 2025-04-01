a373b467343c7f34b647513ee1f424af22c94214
GaryHY <henry.gary@hotmail.com>
2025-04-01 14:58:13 +0200

    feat: add IsEmpty and AsError methods

--- error.go
+++ error.go
@@ -10,10 +10,24 @@ func (m Map) Error() string {
 	if m == nil {
 		return "<nil>"
 	}
+	if m.IsEmpty() {
+		return ""
+	}
 	var builder strings.Builder
 	for field, err := range m {
 		builder.WriteString(fmt.Sprintf("%s: %s; ", field, err.Error()))
 	}
 
 	return builder.String()
+}
+
+func (m Map) IsEmpty() bool {
+	return len(m) == 0
+}
+
+func (m Map) AsError() error {
+	if m.IsEmpty() {
+		return nil
+	}
+	return m
 }
