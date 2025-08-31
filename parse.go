a47d966d7cf8c04e3332890902e56d75d335356e
GaryHY <henry.gary@hotmail.com>
2025-08-31 10:03:33 +0200

    feat: add ParseErrors function to parse error strings back to map

---
+++ parse.go
@@ -0,0 +1,27 @@
+package errsx
+
+import "strings"
+
+// ParseErrors parses an error string back into a map[string]string
+// The input should be in the format: "field1: message1; field2: message2"
+func ParseErrors(s string) map[string]string {
+	result := make(map[string]string)
+	if s == "" {
+		return result
+	}
+	
+	parts := strings.Split(s, "; ")
+	for _, part := range parts {
+		if part == "" {
+			continue
+		}
+		if idx := strings.Index(part, ": "); idx != -1 {
+			key := part[:idx]
+			value := part[idx+2:]
+			if key != "" && value != "" {
+				result[key] = value
+			}
+		}
+	}
+	return result
+}
